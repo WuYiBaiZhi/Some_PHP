f2eb55b14261b5cb05a77aef71b857f9335ebf21
WuYiBaiZhi <wuyibaizhi@gmail.com>
2022-01-22 17:26:50 +0800

    2022/1/22日

---
+++ PHP/1.PHP的基本语法.php
@@ -0,0 +1,23 @@
+<!-- _*_ coding:UTF-8 _*_ -->
+<!-- PROJECT : Some_PHP -->
+<!-- Time : 2022/1/22 17:23 -->
+<!-- Auth : 无意白纸 -->
+<!-- File : 1.PHP的基本语法.php -->
+<!-- IDE  : PhpStorm -->
+<?php
+    echo 'Hello World';
+    echo '<br>';
+    print '我是无意白纸';
+    echo '<br>';
+        // 变量
+    print '<hr>';
+    $a = '你好我叫无意白纸';
+    echo $a;
+    echo '<br>';
+        // 下划线命名法
+    $New_title = 'Hello';
+        // 小驼峰命名法
+    $newTitle = 'World';
+        // 大驼峰命名法
+    $NewTitle = 'WuYiBaiZhi';
+    echo $New_title , $newTitle , ' ' , $NewTitle;
