342d4d2ad10f87732738bbe05ee2c63994a161a1
WuYiBaiZhi <wuyibaizhi@gmail.com>
2022-01-22 18:05:58 +0800

    2022/1/22日

--- PHP/2.PHP标量类型介绍.php
+++ PHP/2.PHP标量类型介绍.php
@@ -5,3 +5,43 @@
 <!-- File : 2.PHP标量类型介绍.php -->
 <!-- IDE  : PhpStorm -->
 <?php
+        //布尔型
+    $x = true;
+    var_dump($x);
+    echo '<hr>';
+    $y = false;
+    var_dump($y);
+    echo '<hr>';
+        // 整形
+    $number = 0;
+    var_dump($number);
+    echo '<hr>';
+    $number = 67;
+    var_dump($number);
+    echo '<hr>';
+    $number = -322;
+    var_dump($number);
+    echo '<hr>';
+        //浮点型
+    $number = 10.03;
+    var_dump($number);
+    echo '<hr>';
+    $number = -88.23;
+    var_dump($number);
+    echo '<hr>';
+        // 字符串
+    $str = '我是无意白纸';
+    var_dump($str);
+    echo '<hr>';
+    $str = 'My name is WuYiBaiZhi';
+    var_dump($str);
+    echo '<hr>';
+        // Null 空值
+    $null;
+    var_dump($null);
+    echo '<hr>';
+    $null = '';
+    var_dump($null);
+    echo '<hr>';
+    $null = null;
+    var_dump($null);
